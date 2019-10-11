60ba7d76b91ad1a8b68f4a92c0b9e53e7c04a394
Andrei Shablyka <kloppp0007@gmail.com>
2019-10-11 10:00:40 +0300

    add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.attempt = Math.round((this.min + this.max) / 2);
+        return this.attempt;
     }
 
     lower() {
-
+        this.max = this.attempt;
     }
 
     greater() {
-
+        this.min = this.attempt;
     }
 }
 
